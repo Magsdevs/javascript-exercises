17f6333fc239d82aad76113e129f4f65599262d6
Manuel Gomez <manu11341@gmail.com>
2023-05-17 01:58:16 +0200

    Completed 3 & 4 Js exercises All test were pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 };
+
+console.log(reverseString('hello there'));
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr) {
+  let args = Array.from(arguments);
+  return arr.slice().filter((val) => {
+    if (!args.includes(val)) {
+      return val;
+    }
+  });
 };
-
+console.log(removeFromArray([1, 2, 3], '1', 3));
 // Do not edit below this line
 module.exports = removeFromArray;
