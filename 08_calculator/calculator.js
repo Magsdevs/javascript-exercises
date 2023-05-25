4e1a39cdcbcb21c445ed145eaba0236bf0ea1043
Manuel Gomez <manu11341@gmail.com>
2023-05-25 06:01:39 +0200

    Exercise 8 and 9 completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function () {
+  let sum = [...arguments].reduce((initial, total) => initial + total);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+  let subtract = [...arguments].reduce((initial, total) => initial - total);
+  return subtract;
 };
 
-const sum = function() {
-	
+const sum = function () {
+  return [...arguments].flat().reduce((initial, total) => initial + total, 0);
 };
 
-const multiply = function() {
-
+const multiply = function () {
+  let args = [...arguments];
+  return args
+    .flat()
+    .reduce((previousValue, currentValue) => previousValue * currentValue);
 };
 
-const power = function() {
-	
+const power = function () {
+  let args = [...arguments];
+  return args.reduce((previousValue, currentValue) =>
+    Math.pow(previousValue, currentValue)
+  );
 };
 
-const factorial = function() {
-	
+const factorial = function () {
+  let [num] = [...arguments];
+  let total = 1;
+
+  for (let i = total; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
@@ -29,5 +44,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
